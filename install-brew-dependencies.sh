f194f96c7474a0e4893f8a8d8a4cab31b714f084
Jeff Burn <jeff.burn@agriwebb.com>
2021-11-03 09:55:29 +1100

    M1 updates

---
+++ install-brew-dependencies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+formula=(jq aws-vault openjdk htop gradle rbenv)
+
+if [[ -f "$(which brew)" ]]; then
+  for i in ${formula[@]}; do
+    if ! brew list "$i" > /dev/null; then
+      echo "➡️  installing $i"
+      brew install "$i"
+    else
+      echo "✅ $i installed"
+    fi
+  done
+else
+  echo "brew not found, skipping setup"
+fi
+
